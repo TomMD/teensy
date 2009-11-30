913a47a6c29dffc5361ee71f01eb9097847ad9f9
Thomas DuBuisson <Thomas.DuBuisson@gmail.com>
2009-11-29 16:29:25 -0800

    Fix up the usb_*_msg calls a little.

--- file_ops.c
+++ file_ops.c
@@ -1,6 +1,5 @@
 /* user-ops.c for the teensy-usb driver
- * Author: Thomas M. DuBuisson
- * License: GPL
+ * License: Dual BSD/GPL
  */
 #include <linux/init.h>
 #include <linux/module.h>
@@ -19,53 +18,99 @@ extern stats_t teensy_stats;
 
 // START /dev/teensy interface
 // 	(read, write, ioctl, open, release)
-teensy_read(struct_file *filp, char __user *buf, size_t count, loff_t *pos)
+ssize_t teensy_read(struct_file *filp, char __user *buf, size_t count,
+		    loff_t *pos)
 {
+	int actualLen=0;
 	struct command cmd;
+	struct teensy_dev *teensy;
 	int err=0;
+
+	teensy = filp->private;
 
 	err = copy_from_user(&cmd, buf, sizeof(struct command));
 	if (err) {
-		printk(KERN_WARNING "teensy-usb: read: Could not copy from user: %d\n", err);
+		printk(KERN_WARNING "teensy-usb: read:"
+			" Could not copy from user: %d\n", err);
 		goto teensy_read_out;
 	}
 
 	if(dat.cmd_type != CMD_READ) {
-		printk(KERN_WARNING "teensy-usb: read: Invalid command %d\n", dat.cmd_type);
-		err = -EINVAL;
+		printk(KERN_WARNING "teensy-usb: read: Invalid command %d\n",
+			dat.cmd_type);
 		goto teensy_read_out;
 	}
 
 	// FIXME issue usb_read(cmd)
+	err = usb_control_msg(teensy->device, usb_sndctrlpipe(teensy->device, teensy->del_endpoint),
+				/*req*/ 0, /*reqType*/ 0, /*val*/ 0, /*idx*/ 0,
+				(void *)&cmd, sizeof(struct command), HZ*10);
+	if (err) {
+		printk(KERN_WARNING "teensy-usb: read:"
+			" Error on control msg: %d\n", err);
+		goto teensy_read_out;
+	}
+	err = usb_bulk_msg(teensy->device, usb_rcvbulkpipe(teensy->device, teensy->read_endpoint), (void *)&cmd.data,
+			   sizeof(cmd.data), &actualLen, HZ*10);
+	if (err) {
+		printk(KERN_WARNING "teensy-usb: read:"
+			" Error on bulk msg: %d\n", err);
+		goto teensy_read_out;
+	}
+
+	err = copy_to_user(buf, (void *)&cmd, sizeof(struct cmd));
+	if (err) {
+		printk(KERN_WARNING "teensy-usb: read:"
+			" Error copying to user buffer: %d\n", err);
+		actualLen=0;
+		goto teensy_read_out;
+	}
 	
 teensy_read_out:
-	return err;
+	return (ssize_t)actualLen;
 }
 
-teensy_write(struct file *filp, const char __user *buf, size_t count, loff_t *pos)
+ssize_t teensy_write(struct file *filp, const char __user *buf, size_t count,
+		     loff_t *pos)
 {
 	struct command cmd;
-	int err=0;
+	int err=0, actualLen=0;
 
 	err = copy_from_user(&cmd, buf, sizeof(struct command));
 	if (err) {
-		printk(KERN_WARNING "teensy-usb: write: Could not copy from user: %d\n", err);
+		printk(KERN_WARNING "teensy-usb: write:"
+			" Could not copy from user: %d\n", err);
 		goto teensy_write_out;
 	}
 
 	if (dat.cmd_type != CMD_STORE) {
-		printk(KERN_WARNING "teensy-usb: write: invalid command: %d\n", dat.cmd_type);
-		err = -EINVAL;
+		printk(KERN_WARNING "teensy-usb: write:"
+			" invalid command: %d\n", dat.cmd_type);
 		goto teensy_write_out;
 	}
 
-	// FIXME perform usb_write(cmd);
+	err = usb_control_msg(teensy->device, usb_sndctrlpipe(teensy->device, teensy->del_endpoint),
+				/*req*/ 0, /*reqType*/ 0, /*val*/ 0, /*idx*/ 0,
+				(void *)&cmd, sizeof(struct command), HZ*10);
+	if (err) {
+		printk(KERN_WARNING "teensy-usb: read:"
+			" Error on control msg: %d\n", err);
+		goto teensy_read_out;
+	}
+	err = usb_bulk_msg(teensy->device, usb_rcvbulkpipe(teensy->device, teensy->write_endpoint), (void *)&cmd.data,
+			   sizeof(cmd.data), &actualLen, HZ*10);
+	if (err) {
+		printk(KERN_WARNING "teensy-usb: read:"
+			" Error on bulk msg: %d\n", err);
+		actualLen=0;
+		goto teensy_read_out;
+	}
 
 teensy_write_out:
-	return err;
+	return (ssize_t)actualLen;
 }
 
-teensy_ioctl(struct inode *inode, struct file *filp, unsigned int cmd, unsigned long idx)
+int teensy_ioctl(struct inode *inode, struct file *filp, unsigned int cmd, unsigned long idx)
 {
 	int res = 0;
 
