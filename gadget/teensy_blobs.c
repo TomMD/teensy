6846a209ecfca62327c7d822508829a6e20ff80d
Thomas DuBuisson <Thomas.DuBuisson@gmail.com>
2009-12-07 21:16:41 -0800

    Formatting to fix with rest of code

--- gadget/teensy_blobs.c
+++ gadget/teensy_blobs.c
@@ -11,35 +11,32 @@ uint64_t total_writes = 0;
 uint64_t bytes_read = 0;
 uint64_t bytes_written = 0;
 
-//PROGMEM blob_t blobs[MAX_BLOBS] = {{0}};
-//blob_t blobs[MAX_BLOBS] PROGMEM = {{0}}; // correct syntax, not writeable
 blob_t blobs[MAX_BLOBS];
 
 uint8_t teensy_store(uint64_t index, uint16_t data)
-  {
-  if ( (index < 0) || (index > MAX_BLOBS) )
-    return EINVALIDINDEX;
-  blobs[index].data = data;
-  return ENOERR;
-  }
+{
+	if ( (index < 0) || (index > MAX_BLOBS) )
+		return EINVALIDINDEX;
+	blobs[index].data = data;
+	return ENOERR;
+}
 
 uint8_t teensy_read(uint64_t index, uint16_t * data)
-  {
-  if ( (index < 0) || (index > MAX_BLOBS) )
-    return EINVALIDINDEX;
-//  memcpy_P( (void*)data, (PGM_VOID_P) blobs[index].data, 2);
-  // , sizeof(blob_t.data));
-  memcpy( (void*) data, (void*) &blobs[index].data, 2);
-  return ENOERR;
-  }
+{
+	if ( (index < 0) || (index > MAX_BLOBS) )
+		return EINVALIDINDEX;
+	//memcpy( (void*) data, (void*) &blobs[index].data, 2);
+	*data = blobs[index].data;
+	return ENOERR;
+}
 
 uint8_t teensy_delete(uint64_t index)
-  {
-  if ( (index < 0) || (index > MAX_BLOBS) )
-    return EINVALIDINDEX;
-  blobs[index].data = 0;
+{
+	if ( (index < 0) || (index > MAX_BLOBS) )
+		return EINVALIDINDEX;
+	blobs[index].data = 0;
 
-  return ENOERR;
-  }
+	return ENOERR;
+}
 
 
